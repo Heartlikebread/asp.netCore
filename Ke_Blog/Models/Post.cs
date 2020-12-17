daec96271f5623d2519c8468c9dd5988457e7769
彥廷 柯 <dku04wu6@gmail.com>
2020-12-17 12:08:33 +0800

    test

---
+++ Ke_Blog/Models/Post.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Ke_Blog.Models
+{
+    public class Post
+    {
+        public string Title { get; set; } = "";
+        public string Body { get; set; } = "";
+        public DateTime Creates { get; set; } = DateTime.Now;
+    }
+}
